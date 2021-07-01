3288bf62c110dcbc267293607fdccc8a3cf2bca5
Ghost_chu <2908803755@qq.com>
2021-07-01 19:23:41 +0800

    避免潜影盒也进行提醒

--- src/main/java/com/bilicraft/resourceswarning/ResourcesWarning.java
+++ src/main/java/com/bilicraft/resourceswarning/ResourcesWarning.java
@@ -1,13 +1,9 @@
 package com.bilicraft.resourceswarning;
 
 import org.apache.commons.lang.StringUtils;
-import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
-import org.bukkit.Material;
-import org.bukkit.Sound;
+import org.bukkit.*;
 import org.bukkit.block.Container;
 import org.bukkit.configuration.Configuration;
-import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -28,14 +24,14 @@ public final class ResourcesWarning extends JavaPlugin implements Listener {
     @Override
     public void onEnable() {
         // Plugin startup logic
-        Bukkit.getPluginManager().registerEvents(this,this);
+        Bukkit.getPluginManager().registerEvents(this, this);
         saveDefaultConfig();
         reloadConfig();
         parseColours(getConfig());
-        this.title =parseColours(getConfig().getString("title"));
-        this.subtitle =parseColours( getConfig().getString("subtitle"));
+        this.title = parseColours(getConfig().getString("title"));
+        this.subtitle = parseColours(getConfig().getString("subtitle"));
         StringBuilder chatBuilder = new StringBuilder();
-        getConfig().getStringList("chat").forEach(str->{
+        getConfig().getStringList("chat").forEach(str -> {
             chatBuilder.append("\n").append(str);
         });
         this.chat = parseColours(chatBuilder.toString());
@@ -46,19 +42,22 @@ public final class ResourcesWarning extends JavaPlugin implements Listener {
     public void onDisable() {
         // Plugin shutdown logic
     }
-    @EventHandler(priority = EventPriority.MONITOR,ignoreCancelled = true)
-    public void onBlockPlace(BlockPlaceEvent event){
-        if(!world.contains(event.getBlockPlaced().getLocation().getWorld().getName())){
+
+    @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
+    public void onBlockPlace(BlockPlaceEvent event) {
+        if (!world.contains(event.getBlockPlaced().getLocation().getWorld().getName())) {
             return;
         }
-       if(event.getBlockPlaced().getState() instanceof Container){
-           if(event.getBlockPlaced().getType() != Material.FURNACE){
-               Player player = event.getPlayer();
-               player.sendTitle(title,subtitle,0,80,0);
-               player.sendMessage(chat);
-               player.playSound(player.getLocation(), Sound.BLOCK_ANVIL_PLACE,1.0f,1.0f);
-           }
-       }
+        if (event.getBlockPlaced().getState() instanceof Container) {
+            if (event.getBlockPlaced().getType() != Material.FURNACE) {
+                if (!Tag.SHULKER_BOXES.isTagged(event.getBlockPlaced().getType())) {
+                    Player player = event.getPlayer();
+                    player.sendTitle(title, subtitle, 0, 80, 0);
+                    player.sendMessage(chat);
+                    player.playSound(player.getLocation(), Sound.BLOCK_ANVIL_PLACE, 1.0f, 1.0f);
+                }
+            }
+        }
     }
 
     public void parseColours(Configuration config) {
@@ -75,6 +74,7 @@ public final class ResourcesWarning extends JavaPlugin implements Listener {
             config.set(key, filtered);
         }
     }
+
     public String parseColours(String text) {
         if (StringUtils.isEmpty(text)) {
             return "";
